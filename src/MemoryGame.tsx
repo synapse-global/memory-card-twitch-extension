8596cef6d98a71cf995ee565b566d580a4e73023
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-01-30 16:45:46 +0300

    fix: scroll

--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -55,7 +55,7 @@ export const MemoryGame = ({
                     bottomRight: false,
                 }}
                 className={cn(
-                    `relative overflow-hidden flex flex-col backdrop-blur-xs transition-colors duration-300 ease-[cubic-bezier(0.25,0.46,0.45,0.94)] bg-red-50 rounded-2xl overflow-y-scroll`,
+                    `relative overflow-hidden flex flex-col backdrop-blur-xs transition-colors duration-300 ease-[cubic-bezier(0.25,0.46,0.45,0.94)] bg-red-50 rounded-2xl`,
                     !isMobile && "rounded-[0.375rem]",
                     isMobile && "flex-1 basis-[0vh]!",
                 )}>
