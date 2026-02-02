4667547e9b58045e155afc89dd0e83902de622c1
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-02-02 11:49:12 +0300

    fix: mobile size

--- src/hooks/useIsMobile.tsx
+++ src/hooks/useIsMobile.tsx
@@ -7,6 +7,18 @@ export const useIsMobile = () => {
         const rootElement = document.getElementById("root");
         const device = rootElement?.dataset.device;
         setIsMobile(device === "mobile");
+
+        if (device === "desk") {
+            document.documentElement.classList.add("font-desk");
+        } else {
+            document.documentElement.classList.remove("font-desk");
+        }
+
+        if (device === "mobile") {
+            document.documentElement.classList.add("font-mobile");
+        } else {
+            document.documentElement.classList.remove("font-mobile");
+        }
     }, []);
     return { isMobile };
 };
